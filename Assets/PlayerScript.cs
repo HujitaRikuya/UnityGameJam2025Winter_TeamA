845f85a8c873b4d394de3c664c449196ff894052
HujitaRikuya <ca01254017@st.kawahara.ac.jp>
2025-12-18 14:14:03 +0900

    プレイヤー操作

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,18 +1,35 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float moveSpeed = 3.0f;
+
+    private Transform m_transform;
+
+    private CharacterController characterController;
+
+   
+    private Vector3 moveVelocity;
+    private void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+
+        m_transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveVelocity.x = Input.GetAxis("Horizontal") * moveSpeed;
+        moveVelocity.y = Input.GetAxis("Vertical") * moveSpeed;
+
+        //transform.LookAt(transform.position + new Vector3(moveVelocity.x, 0, moveVelocity.z));
+
+        characterController.Move(moveVelocity * Time.deltaTime);
+    
     }
 }
