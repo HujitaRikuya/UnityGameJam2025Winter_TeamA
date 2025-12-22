644d00762bcb6a0f0e3bc5c1d581df69de66b4c4
KawaTaro <ca01254006@st.kawahara.ac.jp>
2025-12-22 15:29:49 +0900

    プレイヤーの近くに敵が来たら敵死亡

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -17,16 +17,21 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Slider _expSlider;
 
+    [SerializeField]
+    private Text _levelText;
+
+    private int _currentLv;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     /// <summary>
@@ -40,6 +45,13 @@ public class GameManager : MonoBehaviour
 
     public void UpdateExpSlider()
     {
-        _expSlider.value += 0.02f;
+        _expSlider.value += 0.1f;
+
+        if (_expSlider.value >= 1)
+        {
+            _currentLv++;
+            _levelText.text = _currentLv.ToString();
+            _expSlider.value = 0;
+        }
     }
 }
