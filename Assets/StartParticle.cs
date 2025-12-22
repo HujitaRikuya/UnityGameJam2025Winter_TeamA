dd4701c0b671600b1008a69a0fbbfb4452ff8935
HujitaRikuya <ca01254017@st.kawahara.ac.jp>
2025-12-22 15:32:16 +0900

    エフェクト

---
+++ Assets/StartParticle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartParticle : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("発生させるエフェクト")]
+    private ParticleSystem particle;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision .gameObject.tag=="Enemies")
+        {
+            ParticleSystem newParticle = Instantiate(particle);
+
+            newParticle.transform.position = this.transform.position;
+
+            newParticle.Play();
+
+            Destroy(this.gameObject);
+        
+        }
+    }
+}
