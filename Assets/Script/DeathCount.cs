85d862ec56dfc61357c965f8581c1ddb69cb2512
tonokouti0519 <Ca01254015tonokouti>
2025-12-22 15:33:06 +0900

    あ

---
+++ Assets/Script/DeathCount.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class KillUI : MonoBehaviour
+{
+    public TextMeshProUGUI killText;
+
+    int prevKillCount = -1;
+
+    void Update()
+    {
+        if (killText == null || EnemyScript.DeathCount== null)
+            return;
+
+        int kill = EnemyScript.DeathCount;
+
+        // 値が変わった時だけ更新
+        if (kill != prevKillCount)
+        {
+            killText.text = kill.ToString("000"); // 3桁表示
+            prevKillCount = kill;
+
+            // ちょっと拡大演出（UI感UP）
+            killText.transform.localScale = Vector3.one * 1.2f;
+        }
+
+        // 元のサイズに戻す
+        killText.transform.localScale =
+            Vector3.Lerp(killText.transform.localScale, Vector3.one, 10f * Time.deltaTime);
+    }
+}
---
+++ Assets/Script/Point.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ExpUI : MonoBehaviour
+{
+    public PlayerStats playerStats;
+    public TextMeshProUGUI levelText;
+    public Image expBar;
+
+    void Update()
+    {
+        if (playerStats == null) return;
+
+        // Lv 表示
+        levelText.text = $"Lv.{playerStats.level}";
+
+        // 経験値バー
+        float fill =
+            (float)playerStats.currentExp / playerStats.expToNextLevel;
+        expBar.fillAmount = fill;
+    }
+}
