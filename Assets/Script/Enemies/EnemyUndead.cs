2df61df4e1e8ad9dcd20ad4ffa2e463129ba3ff3
kimurahayato-alpha <kimurahayato0102@gmail.com>
2025-12-22 13:45:45 +0900

    エネミーのスポーンと追跡を実装しました (#13)
    Co-authored-by: KawaTaro <ca01254006@st.kawahara.ac.jp>

---
+++ Assets/Script/Enemies/EnemyUndead.cs
@@ -0,0 +1,15 @@
+﻿
+/// <summary>
+/// アンデット(敵)
+/// </summary>
+public class EnemyUndead : Enemy
+{
+    /// <summary>
+    /// Start
+    /// </summary>
+    void Start()
+    {
+        // ステータス設定
+        SetStatus(30, 1, 2, 15);
+    }
+}
---
+++ Assets/Script/Enemies/EnemyZombie.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// ゾンビ(敵)
+/// </summary>
+public class EnemyZombie : Enemy
+{
+    /// <summary>
+    /// Start
+    /// </summary>
+    private void Start()
+    {
+        // ステータス設定
+        SetStatus(20, 1, 1, 5);
+
+        
+    }
+}
